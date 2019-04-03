417bc2eabd11df263fdc2a83a5cb523a79ba4c8d
Sanket Panda <pandasa123@gmail.com>
2019-04-02 22:29:52 -0400

    Error handling etc

--- tickerHandler.py
+++ tickerHandler.py
@@ -3,37 +3,47 @@ from urllib.request import urlopen
 
 class Ticker:
     def __init__(self, exchangeList=["NASDAQ", "NYSE", "AMEX"], debug=False):
-        self.ticker_dict = get_tickers(exchangeList, debug)
+        self.ticker_dict = self.create_ticker_dict(exchangeList, debug)
+
+    # Input: Company name
     def get_ticker(self, company_name):
+        assert isinstance(company_name, str)
         for ticker, name in self.ticker_dict.items():
             if company_name in name:
                 return ticker
+            else:
+                return None
+
     def get_company_name(self, ticker):
-        return self.ticker_dict[ticker]
-
-
-# Output: dictionary with {ticker, company_name} pairs
-def get_tickers(exchangeList, debug):
-    assert isinstance(exchangeList, list)
-    assert isinstance(debug, bool)
-    ticker_dict = {}
-    for exchange in ["NASDAQ", "NYSE", "AMEX"]:
-        url = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange="
+        assert isinstance(ticker, str)
         try:
-            if debug:
-                print("Downloading tickers from {}...".format(exchange))
-            response = urlopen(url + exchange + '&render=download')
-            content = response.read().decode('utf-8').split('\n')
-            for num, line in enumerate(content):
-                line = line.strip().strip('"').split('","')
-                if num == 0 or len(line) != 9:
-                    continue
-                if line[0] not in ticker_dict.keys():
-                    ticker_dict[line[0]] = line[1]
-            break
+            return self.ticker_dict[ticker]
         except:
-            continue
-    return ticker_dict
+            return None
+
+    # Input: List of exchanges, debug option
+    # Output: dictionary with {ticker, company_name} pairs
+    def create_ticker_dict(self, exchangeList, debug):
+        assert isinstance(exchangeList, list)
+        assert isinstance(debug, bool)
+        ticker_dict = {}
+        for exchange in ["NASDAQ", "NYSE", "AMEX"]:
+            url = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange="
+            try:
+                if debug:
+                    print("Downloading tickers from {}...".format(exchange))
+                response = urlopen(url + exchange + '&render=download')
+                content = response.read().decode('utf-8').split('\n')
+                for num, line in enumerate(content):
+                    line = line.strip().strip('"').split('","')
+                    if num == 0 or len(line) != 9:
+                        continue
+                    if line[0] not in ticker_dict.keys():
+                        ticker_dict[line[0]] = line[1]
+                break
+            except:
+                continue
+        return ticker_dict
 
 # if __name__ == "__main__":
 #     g = Ticker()
