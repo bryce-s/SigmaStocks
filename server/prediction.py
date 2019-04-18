642e0d07ee690e16197f7d5037504015ec06518e
Sanket Panda <pandasa123@gmail.com>
2019-04-17 20:10:08 -0400

    Added the prediction stuff. Need to pair it up with the sentiment, RSS, etc

---
+++ server/prediction.py
@@ -0,0 +1,51 @@
+import numpy as np
+from datetime import datetime
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from iexfinance.stocks import get_historical_data
+
+def getStocks(stockList):
+    for stock in stockList:
+        predictData(stock, 5)
+
+def getMovement(prediction, close):
+    l = len(prediction)
+    # for i in l: 
+        # print(prediction[i] - close)
+    # print(close)
+    # print(prediction)
+
+def predictData(stock, numForecast):
+    start = datetime(2017, 1, 1)
+    end = datetime.now()
+    df = get_historical_data(stock, start=start, end=end, output_format='pandas')
+    if os.path.exists('../Data/StockData'):
+        csv_name = ('../Data/StockData/' + stock + '_Prices.csv')
+    else:
+        os.mkdir("../Data/StockData")
+        csv_name = ('../Data/StockData/' + stock + '_Prices.csv')
+    df.to_csv(csv_name)
+    df['prediction'] = df['close'].shift(-1)
+    df.dropna(inplace=True)
+
+    X = np.array(df.drop(['prediction'], 1))
+    Y = np.array(df['prediction'])
+    X = preprocessing.scale(X)
+    X_prediction = X[-numForecast:]
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.5)
+        
+    clf = LinearRegression()
+    clf.fit(X_train, Y_train)
+    prediction = (clf.predict(X_prediction))
+
+    print(stock)
+    print(prediction)
+    # print(getMovement(prediction, df.tail(1)['close']))
+
+    print('-----------------------------------------------------------------------')
+
+if __name__ == '__main__':
+    getStocks(['GOOG'])
