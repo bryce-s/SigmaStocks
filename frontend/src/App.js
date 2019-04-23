937a2cf1612c897f3660751cff945e4042e89c69
Sanket Panda <pandasa123@gmail.com>
2019-04-23 19:42:34 -0400

    Documentation part two!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,115 +1,118 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css'
 import axios from 'axios'
 import CurrentInvestment from './CurrentInvestment/CurrentInvestment'
-import { Typography, Switch } from 'antd'
-import { Row } from 'react-flexbox-grid'
+import {Switch, Typography} from 'antd'
+import {Row} from 'react-flexbox-grid'
 
-const { Title } = Typography
+const {Title} = Typography;
 
+// Container
 class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			investmentArray: [],
-			portfolioValue: 0,
-			theme: 'light'
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            investmentArray: [],
+            portfolioValue: 0,
+            theme: 'light'
+        }
+    }
 
-	getData() {
-		// console.log('Fetching Data')
-		Promise.all([
-			axios.get(
-				process.env.REACT_APP_HOST +
-					':' +
-					process.env.REACT_APP_SERVERPORT +
-					'/api/stocks'
-			),
-			axios.get(
-				process.env.REACT_APP_HOST +
-					':' +
-					process.env.REACT_APP_SERVERPORT +
-					'/api/portfolio'
-			)
-		]).then(([stocks, portfolio]) => {
-			// console.log('Finished')
-			this.setState({
-				investmentArray: stocks.data,
-				portfolioValue: portfolio.data[0]['portfolioValue']
-			})
-		})
-	}
+    getData() {
+        // console.log('Fetching Data')
+        Promise.all([
+            axios.get(
+                process.env.REACT_APP_HOST +
+                ':' +
+                process.env.REACT_APP_SERVERPORT +
+                '/api/stocks'
+            ),
+            axios.get(
+                process.env.REACT_APP_HOST +
+                ':' +
+                process.env.REACT_APP_SERVERPORT +
+                '/api/portfolio'
+            )
+        ]).then(([stocks, portfolio]) => {
+            // console.log('Finished')
+            this.setState({
+                investmentArray: stocks.data,
+                portfolioValue: portfolio.data[0]['portfolioValue']
+            })
+        })
+    }
 
-	componentDidMount() {
-		// console.log('Mounted')
-		this.getData()
-	}
+    componentDidMount() {
+        // console.log('Mounted')
+        this.getData()
+    }
 
-	changeTheme = value => {
-		const newTheme = value ? 'dark' : 'light'
-		document.body.style.backgroundColor = this.getBackgroundColour(newTheme)
-		this.setState({
-			theme: newTheme
-		})
-	}
+    changeTheme = value => {
+        const newTheme = value ? 'dark' : 'light';
+        document.body.style.backgroundColor = this.getBackgroundColour(newTheme);
+        this.setState({
+            theme: newTheme
+        })
+    };
 
-	getBackgroundColour = newTheme => {
-		if (newTheme === 'dark') {
-			return '#1D2229'
-		}
-		return '#FFF'
-	}
+    getBackgroundColour = newTheme => {
+        if (newTheme === 'dark') {
+            return '#1D2229'
+        }
+        return '#FFF'
+    };
 
-	getTitleColour() {
-		if (this.state.theme === 'dark') {
-			return '#FFF'
-		}
-		return '#000'
-	}
+    getTitleColour() {
+        if (this.state.theme === 'dark') {
+            return '#FFF'
+        }
+        return '#000'
+    }
 
-	render() {
-		// console.log('Rendered')
-		console.log(this.state)
-		return (
-			<div
-				className="app-container"
-				style={{
-					textAlign: 'center',
-					marginTop: '20%'
-				}}
-			>
-				<Title level={1} style={{ color: this.getTitleColour() }}>
-					${this.state.portfolioValue}
-				</Title>
+    render() {
+        // Render portfolio value and number of cards based on JSON response
 
-				<Switch
-					checked={this.state.theme === 'dark'}
-					onChange={this.changeTheme}
-					checkedChildren="Dark"
-					unCheckedChildren="Light"
-					style={{ marginBottom: '1%' }}
-				/>
+        // console.log('Rendered')
+        console.log(this.state);
+        return (
+            <div
+                className="app-container"
+                style={{
+                    textAlign: 'center',
+                    marginTop: '20%'
+                }}
+            >
+                <Title level={1} style={{color: this.getTitleColour()}}>
+                    ${this.state.portfolioValue}
+                </Title>
 
-				<Row
-					id="currentStocks"
-					style={{
-						marginLeft: '1%',
-						marginRight: '1%'
-					}}
-				>
-					{this.state.investmentArray.map((currStock, index) => (
-						<CurrentInvestment
-							key="stock_{index}"
-							ticker={currStock.ticker}
-							movement={currStock.movement}
-							theme={this.state.theme}
-						/>
-					))}
-				</Row>
-			</div>
-		)
-	}
+                <Switch
+                    checked={this.state.theme === 'dark'}
+                    onChange={this.changeTheme}
+                    checkedChildren="Dark"
+                    unCheckedChildren="Light"
+                    style={{marginBottom: '1%'}}
+                />
+
+                <Row
+                    id="currentStocks"
+                    style={{
+                        marginLeft: '1%',
+                        marginRight: '1%'
+                    }}
+                >
+                    {this.state.investmentArray.map((currStock, index) => (
+                        <CurrentInvestment
+                            key="stock_{index}"
+                            ticker={currStock.ticker}
+                            movement={currStock.movement}
+                            theme={this.state.theme}
+                        />
+                    ))}
+                </Row>
+            </div>
+        )
+    }
 }
 
 export default App
--- frontend/src/CurrentInvestment/CurrentInvestment.js
+++ frontend/src/CurrentInvestment/CurrentInvestment.js
@@ -1,88 +1,91 @@
-import React, { Component } from 'react'
-import { Statistic, Card, Icon } from 'antd'
-import { Col } from 'react-flexbox-grid'
-import candlestickModal from './candlestickModal.js'
+import React, {Component} from 'react'
+import {Card, Icon, Statistic} from 'antd'
+import {Col} from 'react-flexbox-grid'
+// import candlestickModal from './candlestickModal.js'
 
+
+// CurrentInvestment object
 export default class CurrentInvestment extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			movementDirection: this.props.movement[0]
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            movementDirection: this.props.movement[0]
+        }
+    }
 
-	getArrowType() {
-		if (this.state.movementDirection === '-') {
-			return 'arrow-down'
-		} else if (this.state.movementDirection === '+') {
-			return 'arrow-up'
-		} else {
-			return 'minus'
-		}
-	}
+    getArrowType() {
+        if (this.state.movementDirection === '-') {
+            return 'arrow-down'
+        } else if (this.state.movementDirection === '+') {
+            return 'arrow-up'
+        } else {
+            return 'minus'
+        }
+    }
 
-	getStockValueColour() {
-		if (this.props.theme === 'dark') {
-			if (this.state.movementDirection === '-') {
-				return '#EB999A'
-			} else if (this.state.movementDirection === '+') {
-				return '#93CF9C'
-			} else {
-				return '#67A9ED'
-			}
-		}
-		if (this.state.movementDirection === '-') {
-			return '#cf1322'
-		} else if (this.state.movementDirection === '+') {
-			return '#3f8600'
-		} else {
-			return '#ECECEC'
-		}
-	}
+    getStockValueColour() {
+        if (this.props.theme === 'dark') {
+            if (this.state.movementDirection === '-') {
+                return '#EB999A'
+            } else if (this.state.movementDirection === '+') {
+                return '#93CF9C'
+            } else {
+                return '#67A9ED'
+            }
+        }
+        if (this.state.movementDirection === '-') {
+            return '#cf1322'
+        } else if (this.state.movementDirection === '+') {
+            return '#3f8600'
+        } else {
+            return '#ECECEC'
+        }
+    }
 
-	getCardBackgroundColour() {
-		if (this.props.theme === 'dark') {
-			return '#4C5160'
-		}
-		return '#FFF'
-	}
+    getCardBackgroundColour() {
+        if (this.props.theme === 'dark') {
+            return '#4C5160'
+        }
+        return '#FFF'
+    }
 
-	getTitleColour() {
-		if (this.props.theme === 'dark') {
-			return '#FFF'
-		}
-		return '#000'
-	}
+    getTitleColour() {
+        if (this.props.theme === 'dark') {
+            return '#FFF'
+        }
+        return '#000'
+    }
 
-	getValue(movement) {
-		return movement.substring(1)
-	}
+    getValue(movement) {
+        return movement.substring(1)
+    }
 
-	showCandlestick() {
-		alert("I tried");
-	}
+    showCandlestick() {
+        alert("I tried");
+    }
 
-	render() {
-		return (
-			<Col xs style={{ marginBottom: '3%', marginTop: '3%' }}>
-				<Card
-					style={{ backgroundColor: this.getCardBackgroundColour() }}
-					onClick={this.showCandlestick}
-				>
-					<Statistic
-						title={this.props.ticker}
-						value={this.getValue(this.props.movement)}
-						precision={2}
-						valueStyle={{ color: this.getStockValueColour() }}
-						prefix={<Icon type={this.getArrowType()} />}
-						suffix="%"
-						style={{
-							marginLeft: '-1%',
-							color: this.getTitleColour()
-						}}
-					/>
-				</Card>
-			</Col>
-		)
-	}
+    render() {
+        // Rendering a card with style decided
+        return (
+            <Col xs style={{marginBottom: '3%', marginTop: '3%'}}>
+                <Card
+                    style={{backgroundColor: this.getCardBackgroundColour()}}
+                    onClick={this.showCandlestick}
+                >
+                    <Statistic
+                        title={this.props.ticker}
+                        value={this.getValue(this.props.movement)}
+                        precision={2}
+                        valueStyle={{color: this.getStockValueColour()}}
+                        prefix={<Icon type={this.getArrowType()}/>}
+                        suffix="%"
+                        style={{
+                            marginLeft: '-1%',
+                            color: this.getTitleColour()
+                        }}
+                    />
+                </Card>
+            </Col>
+        )
+    }
 }
